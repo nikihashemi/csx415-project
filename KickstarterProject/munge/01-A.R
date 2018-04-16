13e6d81a987d88cd34e6965202f687d796c82bf0
Hashemi <hashemin@nala.roche.com>
2018-04-15 20:19:48 -0700

    Additions to Project

--- KickstarterProject/munge/01-A.R
+++ KickstarterProject/munge/01-A.R
@@ -1,1 +1,38 @@
-# Example preprocessing script.
+#Preprocessing script
+
+#Add rows to 2018 dataset
+ks.projects.201801["profit"] <- NA
+ks.projects.201801$profit <- ks.projects.201801$usd_pledged_real - ks.projects.201801$usd_goal_real
+
+ks.projects.201801["profit_success"] <- NA
+ks.projects.201801$profit_success <- ifelse(ks.projects.201801$profit > 0,1,0)
+
+ks.projects.201801["amount.per.backer"] <- NA
+ks.projects.201801$amount.per.backer <- ks.projects.201801$usd_pledged_real/ks.projects.201801$backers
+
+ks.projects.201801["campaign.length"] <- NA
+ks.projects.201801$deadline <- as.Date(strptime(ks.projects.201801$deadline, "%m/%d/%Y"))
+ks.projects.201801$launched <- as.Date(strptime(ks.projects.201801$launched, "%m/%d/%Y"))
+ks.projects.201801$campaign.length <- ks.projects.201801$deadline - ks.projects.201801$launched
+
+ks.projects.201801["succ"] <- NA
+ks.projects.201801$succ <- ifelse(ks.projects.201801$state == "successful",1,0)
+
+
+#Add rows to 2016 dataset
+ks.projects.201612["profit"] <- NA
+ks.projects.201612$profit <- ks.projects.201612$pledged - ks.projects.201612$goal
+
+ks.projects.201612["profit_success"] <- NA
+ks.projects.201612$profit_success <- ifelse(ks.projects.201612$profit > 0,1,0)
+
+ks.projects.201612["amount.per.backer"] <- NA
+ks.projects.201612$amount.per.backer <- ks.projects.201612$pledged/ks.projects.201612$backers
+
+ks.projects.201612["campaign.length"] <- NA
+ks.projects.201612$deadline <- as.Date(strptime(ks.projects.201612$deadline, "%m/%d/%Y"))
+ks.projects.201612$launched <- as.Date(strptime(ks.projects.201612$launched, "%m/%d/%Y"))
+ks.projects.201612$campaign.length <- ks.projects.201612$deadline - ks.projects.201612$launched
+
+ks.projects.201612["succ"] <- NA
+ks.projects.201612$succ <- ifelse(ks.projects.201612$state == "successful",1,0)
--- KickstarterProject/src/eda.R
+++ KickstarterProject/src/eda.R
@@ -1,8 +1,52 @@
-library('ProjectTemplate')
-load.project()
+#First Look at dataset
+head(ks.projects.201801)
+summary(ks.projects.201801)
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+
+#First look at categorical variables
+#Create bar graph for frequency in main_category column limited to the top 15 categories
+cat.freq <- ks.projects.201801 %>% group_by(main_category) %>% summarize(count = n()) %>% arrange(desc(count))
+sm.cat.freq <- cat.freq[1:15,1:2]
+sm.cat.freq.plot <- ggplot(sm.cat.freq, aes(reorder(main_category, -count), count, fill=count)) + geom_bar(stat="identity") + xlab("Project Category") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5))
+sm.cat.freq.plot
+
+#Create bar graph for frequency in state column
+state.freq <- ks.projects.201801 %>% group_by(state) %>% summarize(count = n()) %>% arrange(desc(count))
+sm.state.freq <- state.freq[1:6,1:2]
+sm.state.freq.plot <- ggplot(sm.state.freq, aes(reorder(state, -count), count, fill=count)) + geom_bar(stat = "identity") + xlab("State") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5))
+sm.state.freq.plot
+
+#Look into Film & Video group and break into subcategories
+subcat.freq.fv <- ks.projects.201801 %>% filter(main_category =="Film & Video") %>% group_by(category) %>% summarize(count = n()) %>% arrange(desc(count))
+sm.subcat.freq.fv <- subcat.freq.fv[1:5,1:2]
+sm.subcat.freq.fv.plot <- ggplot(sm.subcat.freq.fv, aes(reorder(category, -count), count, fill = count)) + geom_bar(stat = "identity") + xlab("Film & Video Subcategories") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+sm.subcat.freq.fv.plot
+
+#Look into Music group and break into subcategories
+subcat.freq.m <- ks.projects.201801 %>% filter(main_category =="Music") %>% group_by(category) %>% summarize(count = n()) %>% arrange(desc(count))
+sm.subcat.freq.m <- subcat.freq.m[1:5,1:2]
+sm.subcat.freq.m.plot <- ggplot(sm.subcat.freq.m, aes(reorder(category, -count), count, fill = count)) + geom_bar(stat = "identity") + xlab("Music Subcategories") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+sm.subcat.freq.m.plot
+
+#Look into failed state group and break into subcategories
+failed.freq <- ks.projects.201801 %>% filter(state == "failed") %>% group_by(main_category) %>% summarize(count = n()) %>% arrange(desc(count))
+failed.freq.plot <- ggplot(failed.freq, aes(reorder(main_category, -count), count, fill = count)) + geom_bar(stat = "identity") + xlab("Failed Project Categories") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+failed.freq.plot
+
+#Look into successful state group and break into subcategories
+success.freq <- ks.projects.201801 %>% filter(state == "successful") %>% group_by(main_category) %>% summarize(count = n()) %>% arrange(desc(count))
+success.freq.plot <- ggplot(success.freq, aes(reorder(main_category, -count), count, fill = count)) + geom_bar(stat = "identity") + xlab("Successful Project Categories") + ylab("Frequency") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+success.freq.plot
+
+
+#First linear model: Naive Model
+lm.result <- lm(ks.projects.201801$usd_pledged_real~ks.projects.201801$backers, data = ks.projects.201801)
+summary(lm.result)
+#First model: total pledge amount = 1088.4658 + 75.46353*Number of Backers
+#p-value < 0.01 which means that the number of backers has a significant affect on the total pledge amount. The slope is ~75 which means
+#that each individual on kickstarter contributed an average of $75
+
+
+
+
+
