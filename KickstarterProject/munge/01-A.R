581e6b6216e15f0171da50f7257ab880fcb81af6
Hashemi <hashemin@nala.roche.com>
2018-05-03 20:15:32 -0700

    Update data cleaning script

--- KickstarterProject/munge/01-A.R
+++ KickstarterProject/munge/01-A.R
@@ -1,16 +1,25 @@
 #Preprocessing script
 
 #Re-name datasets
-train <- ks.projects.201612
-test <- ks.projects.201801 
+ks <- ks.projects.201801 
 
-#Turn deadline column into date column
-mydate <- mdy_hm(as.character(train$deadline))
-train$year <- year(mydate)
+#Add percent funded column to dataset
+ks$percent.funded <- NA
+for(i in 1:length(ks$deadline)) {
+  ks$percent.funded[i] <- round(ks$usd_pledged_real[i]/ks$usd_goal_real[i], digits = 2)
+  print(i)
+}
 
-mydate2 <- mdy(as.character(test$deadline))
-test$year <- year(mydate2)
+#Add percent funded category column to dataset
+ks$percent.funded.cat <- NA
+for(k in 1:length(ks$deadline)) {
+  if(ks$percent.funded[k] >= 0.81 & ks$percent.funded[k] < 1.00) {
+    ks$percent.funded.cat[k] <- "yes"
+    print(k)
+  }
+  else {
+    ks$percent.funded.cat[k] <- "no"
+    print(k)
+  }
+}
 
-#update train and test datasets based on year
-train <- train[ which(train$year <= '2016'), ]
-test <- test[which(test$year == '2017'), ]
