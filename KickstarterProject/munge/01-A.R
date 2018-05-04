8e1c5dd4cc3e6a967b0ae5e88bba0cbd12957229
Hashemi <hashemin@nala.roche.com>
2018-05-03 23:26:50 -0700

    Update munge script to add new columns to data

--- KickstarterProject/munge/01-A.R
+++ KickstarterProject/munge/01-A.R
@@ -1,25 +1,37 @@
-#Preprocessing script
+#Preprocessing script and addition of columns
 
 #Re-name datasets
 ks <- ks.projects.201801 
 
 #Add percent funded column to dataset
 ks$percent.funded <- NA
-for(i in 1:length(ks$deadline)) {
+for(i in 1:length(ks$ID)) {
   ks$percent.funded[i] <- round(ks$usd_pledged_real[i]/ks$usd_goal_real[i], digits = 2)
   print(i)
 }
 
-#Add percent funded category column to dataset
-ks$percent.funded.cat <- NA
-for(k in 1:length(ks$deadline)) {
-  if(ks$percent.funded[k] >= 0.81 & ks$percent.funded[k] < 1.00) {
-    ks$percent.funded.cat[k] <- "yes"
+#Add funded over 20 percent column to dataset
+ks$over20 <- NA
+for(k in 1:length(ks$ID)) {
+  if(ks$percent.funded[k] > 0.20 & ks$percent.funded[k] < 1.00) {
+    ks$over20[k] = 1
     print(k)
   }
   else {
-    ks$percent.funded.cat[k] <- "no"
+    ks$over20[k] = 0
     print(k)
+  }
+}
+
+ks$failed <- NA
+for(j in 1:length(ks$ID)) {
+  if(ks$state[j] != "success") {
+    ks$failed[j] = 1
+    print(j)
+  }
+  else {
+    ks$failed[j] = 0
+    print(j)
   }
 }
 
