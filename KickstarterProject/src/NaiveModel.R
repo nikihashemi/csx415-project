94d96b8da02cc614377c8953e1e382cf69d4c806
Hashemi <hashemin@nala.roche.com>
2018-05-05 17:51:02 -0700

    Updates to Project Template

--- KickstarterProject/src/NaiveModel.R
+++ KickstarterProject/src/NaiveModel.R
@@ -2,20 +2,16 @@
 #Part 1: Predict failure
 #Naive model is that project will fail
 
-ks_final$state[ks_final$state != "successful"] <- 1
-ks_final$state[ks_final$state == "successful"] <- 0
-ks_final$state <- as.numeric(ks_final$state)
-ks_final.train$state <- as.numeric(ks_final.train$state)
-ks_final.test$state <- as.numeric(ks_final.test$state)
-
-fit <- glm(formula = ks_final.train$state ~ 1, data = ks_final.train, family = binomial)
-output <- predict(fit, ks_final.test)
-
-postResample(pred = 1, obs = output)
+fit1 <- glm(formula = train$state.failed ~ 1, data = train, family = binomial)
+predict1 <- predict(fit1, test)
+postResample(pred = predict1, obs = test$state.failed)
 
 
 #Part 2: Predict project will be over 20% funded
 #Naive model is that project will be over 20% funded
 
+fit2 <- glm(formula = train2$over20 ~ 1, data = train2, family = binomial)
+predict2 <- predict(fit2, test2)
+postResample(pred = predict2, obs = test2$over20)
 
 
--- KickstarterProject/src/TestTrainSplit.R
+++ KickstarterProject/src/TestTrainSplit.R
@@ -1,8 +1,17 @@
-#Splitting data between test and training sets using caret
+#Splitting data between test and training sets using caret for Part 1
 set.seed(3456)
-trainIndex <- createDataPartition(ks_final$state[ks_final$state == "failed"], p = .8, list = FALSE, times = 1)
+trainIndex <- createDataPartition(ks_final$state.failed, p = .8, list = FALSE, times = 1)
 head(trainIndex)
 
-ks_final.train <- ks_final[trainIndex,]
-ks_final.test <- ks_final[-trainIndex,]
+train <- ks_final[trainIndex,]
+test <- ks_final[-trainIndex,]
 
+
+#Splitting data between test and training sets using caret for Part 2
+ks_final2 <- subset(ks_final, state.failed == 1, select = X:state.failed)
+set.seed(3456)
+trainIndex2 <- createDataPartition(ks_final2$over20, p = 0.8, list = FALSE, times = 1)
+head(trainIndex2)
+
+train2 <- ks_final2[trainIndex2,]
+test2 <- ks_final2[-trainIndex2,]
