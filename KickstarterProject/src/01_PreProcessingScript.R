8335985e0d9f7ffb1813348b8e9305b6ddd4b633
Hashemi <hashemin@nala.roche.com>
2018-05-12 11:20:38 -0700

    Update pre processing script

--- KickstarterProject/src/01_PreProcessingScript.R
+++ KickstarterProject/src/01_PreProcessingScript.R
@@ -6,6 +6,9 @@ setwd('C:/Users/hashemin/csx415-project/KickstarterProject/data')
 #rename dataset
 ks <- read.csv('ks-projects-201801.csv')
 
+#Check for NA values
+sapply(ks, function(x) sum(is.na(x)))
+
 #Add columns to dataset
 ks$percent.funded <- round(ks$usd_pledged_real/ks$usd_goal_real, digits = 2)
 ks$over20 <- ifelse(ks$percent.funded >= 0.20, 1, 0)
@@ -22,5 +25,6 @@ library('tidyverse')
 library('caret')
 library('gbm')
 library('devtools')
+library('roxygen2')
 library('testthat')
 
