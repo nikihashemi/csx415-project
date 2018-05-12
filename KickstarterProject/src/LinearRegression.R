e5a900f9af8c44717adebe070568bfe58c83e3fc
Hashemi <hashemin@nala.roche.com>
2018-05-12 13:05:26 -0700

    Update Linear Regression Models

--- KickstarterProject/src/LinearRegression.R
+++ KickstarterProject/src/LinearRegression.R
@@ -1,12 +1,11 @@
-# Linear Regression Models
-train$pledged[train$pledged == 0] <- 1
-train$backers[train$backers == 0] <- 1
-lin.reg <- lm(log(pledged) ~ log(backers) + goal + main_category + year, data = train) 
-summary(lin.reg)
+#Part 1: Basic linear regression model 
+linear.fit1 <- lm(formula = train1$over20 ~ main_category + usd_goal_real + backers + country + state.failed, data = train1)
+linear.predict1 <- predict(linear.fit1, test1)
+postResample(pred = linear.predict1, obs = test1$over20)
 
-dist.predict <- predict(lin.reg, test)
-actuals_preds <- data.frame(cbind(actuals = log(train$pledged), predicteds = dist.predict))
-correlation_accuracy <- cor(actuals_preds)
-min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))
+#Part 2: Basic linear regression model 
+linear.fit2 <- lm(formula = train2$state.failed ~ main_category + usd_goal_real + backers + country, data = train2)
+linear.predict2 <- predict(linear.fit2, test2)
+postResample(pred = linear.predict2, obs = test2$state.failed)
 
 
