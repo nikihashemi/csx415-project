e534ddea6acc541281b4d525c3c77aa087feb7a0
Hashemi <hashemin@nala.roche.com>
2018-05-14 19:15:50 -0700

    Update pkgs directory

--- KickstarterProject/pkgs/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(pkgs)
-
-test_check("pkgs")
--- KickstarterProject/pkgs/tests/testthat/TestGradientBoost.R
+++
@@ -1,3 +0,0 @@
-#Test that over20 and state.failed columns are numeric
-expect_is(test1$over20, "integer")
-expect_is(test2$state.failed, "integer")
