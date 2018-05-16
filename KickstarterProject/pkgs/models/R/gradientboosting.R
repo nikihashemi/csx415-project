4ff7c0d61ec14968d6aedd6ccf59fa7455351f27
Hashemi <hashemin@nala.roche.com>
2018-05-15 18:45:36 -0700

    Update models package

--- KickstarterProject/pkgs/models/R/gradientboosting.R
+++ KickstarterProject/pkgs/models/R/gradientboosting.R
@@ -4,10 +4,15 @@ fitControl <- trainControl(method = "repeatedcv", number = 5, repeats = 5)
 set.seed(825)
 gbmFit1 <- train(over20 ~ main_category + usd_goal_real + backers + country + state.failed, 
                  data = train1, method = "gbm", trControl = fitControl, verbose = TRUE)
-
+gbmFit1
+advanced.predict1 <- predict(gbmFit1, test1)
+postResample(pred = advanced.predict1, obs = test1$over20)
 
 #Part 2: if project is over 20 percent funded, predict that the project will fail
 fitControl <- trainControl(method = "repeatedcv", number = 5, repeats = 5)
 set.seed(825)
 gbmFit2 <- train(state.failed ~ main_category + usd_goal_real + backers + country, 
                  data = train2, method = "gbm", trControl = fitControl, verbose = TRUE)
+gbmFit2
+advanced.predict2 <- predict(gbmFit2, test2)
+postResample(pred = advanced.predict2, obs = test2$state.failed)
